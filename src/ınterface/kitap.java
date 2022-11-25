d72a7126e5fbf2b85f46b92bf214aa94539f70e3
süleyman demirel <55292913+Fahreyn@users.noreply.github.com>
2022-11-25 10:53:51 +0300

    Add files via upload

---
+++ src/ınterface/kitap.java
@@ -0,0 +1,11 @@
+package ýnterface;
+
+interface kitap {
+    String kitapAdý = "insanlýk";
+    int syf = 189;
+    
+    //gosterilmesi için soyut metot oluþturduk 
+    void goster();
+}
+
+   
---
+++ src/ınterface/test.java
@@ -0,0 +1,16 @@
+package ınterface;
+
+public class test {
+
+
+		public static void main(String[] args) {
+			// arayuz classından nesnemizi oluşturduk
+			arayuz arayuz1 = new arayuz();
+
+			arayuz1.goster();
+			
+
+		}
+
+	}
+
